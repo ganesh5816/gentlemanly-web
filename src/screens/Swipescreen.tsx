a8c7f63f79b0b2f9aaddd42b699ae51f56f4ad2e
Ganes769 <ganesh@skillrank.io>
2025-08-26 10:36:51 +0100

    eslit fix

--- src/screens/Swipescreen.tsx
+++ src/screens/Swipescreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { X, Check, ArrowLeft, ShoppingBag } from "lucide-react";
 import { motion, AnimatePresence } from "framer-motion";
 
